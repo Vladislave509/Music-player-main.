c496771a0ad4258cda080a4582a78634980bac12
Limbo89 <gmd_2006@mail.ru>
2022-04-17 19:57:47 +0700

    17.04.22

--- server.js
+++ server.js
@@ -1,11 +1,46 @@
-const PORT = 5000;
+const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 const app = express();
+const urlencodedParser = express.urlencoded({extended: false});
 
-app.get('/', (req, res) => {
+async function getdata(query) {
+    let dataq = {
+        authorization:"SELECT * FROM users",
+    }
+    let db = new sqlite3.Database('music-player.db');
+    var promise = new Promise(function (resolve, reject) {
+        db.all(dataq[query], function (err, row) {
+            if (err) {
+                reject(console.log('ошибо4ка'));
+            } else {
+                resolve(row);
+            }
+        });
+    });
+    let rows = await promise;
+    db.close();
+    return rows
+};
+
+app.get("/", urlencodedParser, (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(5000, () => {
+app.post("/auth", urlencodedParser, (req, res) => {
+    password = req.body.pass;
+    username = req.body.username;
+    body = "authorization";
+    getdata(body).then((rows) => {
+        console.log(rows);
+        console.log(username);
+        console.log(password);
+    });
+});
+
+app.listen(5000, urlencodedParser ,() => {
     console.log('Server started');
 });
+
+// body = "";
+// getdata(body).then((rows) => {
+// });
