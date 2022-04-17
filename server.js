16697963f698086145fd8a987ca35285ce3e13ea
Limbo89 <gmd_2006@mail.ru>
2022-04-17 22:34:53 +0700

    17.04.22

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const sqlite3 = require('sqlite3').verbose();
+const { request } = require('express');
 const express = require('express');
 const app = express();
 const urlencodedParser = express.urlencoded({extended: false});
 
-async function getdata(query) {
+async function getdata(query, login, pass) {
     let dataq = {
-        authorization:"SELECT * FROM users",
+        authorization:"SELECT * FROM users WHERE password = " + pass + " and username = " + login,
     }
     let db = new sqlite3.Database('music-player.db');
     var promise = new Promise(function (resolve, reject) {
         db.all(dataq[query], function (err, row) {
             if (err) {
-                reject(console.log('ошибо4ка'));
+                reject();
             } else {
                 resolve(row);
             }
@@ -29,11 +30,9 @@ app.get("/", urlencodedParser, (req, res) => {
 app.post("/auth", urlencodedParser, (req, res) => {
     password = req.body.pass;
     username = req.body.username;
-    body = "authorization";
-    getdata(body).then((rows) => {
+    body__req = "authorization";
+    getdata(body__req, username, password).then((rows) => {
         console.log(rows);
-        console.log(username);
-        console.log(password);
     });
 });
 
